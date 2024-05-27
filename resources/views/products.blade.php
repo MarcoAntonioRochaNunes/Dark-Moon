c131a976f672b3d99c41914fa275e49b7e67e498
Marco Antonio <marco.nunes@hfa.mil.br>
2024-05-27 13:39:45 -0300

    alteraçoes e nova pag

---
+++ resources/views/products.blade.php
@@ -0,0 +1,8 @@
+@include('header')
+<div class="">
+    teste
+</div>
+
+
+
+@include('footer')
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ProdutosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index']);
+Route::get('/product/{id}', [ProdutosController::class, 'index']);
 
